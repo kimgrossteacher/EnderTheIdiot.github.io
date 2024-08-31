b6f6a5286022dbba5af2c5acc7f10dc0bea77265
EnderTheIdiot <143972698+EnderTheIdiot@users.noreply.github.com>
2024-08-30 18:16:33 -0600

    Add files via upload

---
+++ Hello World Test 3/HelloWorld.java
@@ -0,0 +1,11 @@
+
+
+class Hello {
+
+    public static void main (String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("My name is Henry Smith and I am 16 years old.");
+        System.out.println("My parent's names are Amy and Jesse.");
+        System.out.println("My sister is named Hazel.");
+    }
+}
